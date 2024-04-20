a66dcd9e4d6d6d5f05bd84e654d581d1ca26c7ba
kesava moorthy Palanisamy <kesavamoorthybe@gmail.com>
2024-04-20 12:10:46 +0700

    adding simple WebTestClient  testing

---
+++ reactiveproject-1/src/test/java/com/hippo/reactiveproject/webclientTest/A_SimpleWebTestClientTest.java
@@ -0,0 +1,37 @@
+package com.hippo.reactiveproject.webclientTest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import com.hippo.reactiveproject.models.CustomResponse;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class A_SimpleWebTestClientTest {
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void reactiveMathSquareStepVerifierTest() {
+        Flux<CustomResponse> responseFlux = webClient.get().uri("/math/reactive/square/{input}", 5)
+                .exchange().expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(CustomResponse.class).getResponseBody();
+        StepVerifier.create(responseFlux).expectNextMatches(r -> r.getOutputValue() == 25)
+                .verifyComplete();
+    }
+
+    @Test
+    public void reactiveMathFluentAssertionTest() {
+        webClient.get().uri("/math/reactive/square/{input}", 5).exchange().expectHeader()
+                .contentType(MediaType.APPLICATION_JSON).expectBody(CustomResponse.class)
+                // .isEqualTo(null) --> like this also can be used
+                .value(v -> Assertions.assertThat(v.getOutputValue()).isEqualTo(25));
+    }
+    
+}
